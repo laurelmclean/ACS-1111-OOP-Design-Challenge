6dc750019085d6dca15003ba4f097d2300000020
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-28 19:08:47 -0500

    Created a Player class that inherits from GameObject

---
+++ player.py
@@ -0,0 +1,25 @@
+import pygame
+from game_object import GameObject
+
+# This class extends GameObject
+class Player(GameObject):
+    # method 1
+    def __init__(self, x, y, image, health):
+        super().__init__(x, y, image)
+        self.starting_health = health
+        self.remaining_health = health
+
+    # method 2
+    def update_position(self):
+        # Update the player's position based on keys pressed
+        pass
+
+    # method 3
+    def update_health(self):
+        # Updates the player's health based on scoring and receiving laser blasts
+        pass
+
+
+
+
+    
